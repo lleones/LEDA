e7afdddc192fa2650f163aa6e4b64623f08ddd2b
Lucas Leones <96065550+lleones@users.noreply.github.com>
2022-10-15 16:06:40 -0300

    adicionando rr04

---
+++ RR3-01-Roteiro-Revisao-JUnit-environment/src/test/java/java/TestRepositorioProdutos.java
@@ -0,0 +1,12 @@
+package java;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestRepositorioProdutos {
+
+	@Test
+	public void testerrado() {
+		assertEquals(1, 1);
+	}
+}
