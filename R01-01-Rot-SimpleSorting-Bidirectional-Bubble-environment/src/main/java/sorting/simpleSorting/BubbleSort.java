7ac981552f6cf1d977f7d4ba117dbabfd7c37846
lleones <lucas.leones.santos@ccc.ufcg.edu.br>
2022-10-18 09:50:16 -0300

    boubble sort

---
+++ R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/simpleSorting/BubbleSort.java
@@ -0,0 +1,28 @@
+package sorting.simpleSorting;
+
+import sorting.AbstractSorting;
+
+import util.Util;
+
+/**
+ * The bubble sort algorithm iterates over the array multiple times, pushing big
+ * elements to the right by swapping adjacent elements, until the array is
+ * sorted.
+ */
+public class BubbleSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		boolean trocou = true;
+		while (trocou) {
+			trocou = false;
+			for (int i = leftIndex; i < rightIndex; i++) {
+				if (array[i].compareTo(array[i + 1]) > 0) {
+					Util.swap(array, i, i + 1);
+					trocou = true;
+				}
+			}
+		}
+
+	}
+}
