7cb15c72080d47cf280eb558ed0b7c195ef85a61
lleones <lucas.leones.santos@ccc.ufcg.edu.br>
2022-11-03 10:36:22 -0300

    Início roteiro 4 :)

--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -12,70 +12,36 @@ public class ExtendedCountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		if (rightIndex >= array.length || leftIndex >= rightIndex || array == null) {
-			return;
-		}
+		if (!(array == null || rightIndex >= array.length || leftIndex >= rightIndex || leftIndex < 0)) {
+			int indexMaior = indexMaior(array, leftIndex, rightIndex);
+			int indexMenor = indexMenor(array, leftIndex, rightIndex);
 
-		int indexMaior = indexMaior(array, leftIndex, rightIndex);
-		int indexMenor = indexMenor(array, leftIndex, rightIndex);
+			int[] c = new int[array[indexMaior] + Math.abs(array[indexMenor]) + 1];
 
-		int[] c = new int[array[indexMaior] + Math.abs(array[indexMenor]) + 1];
+			// Ocorrencias:
+			for (int i = leftIndex; i <= rightIndex; i++) {
+				c[array[i] + Math.abs(array[indexMenor])]++;
+			}
 
-		// Ocorrencias:
-		for (int i = leftIndex; i <= rightIndex; i++) {
-			c[array[i] + Math.abs(array[indexMenor])]++;
-		}
+			// Cumulativa:
+			for (int i = 1; i < c.length; i++) {
+				c[i] += c[i - 1];
+			}
 
-		// Cumulativa:
-		for (int i = 1; i < c.length; i++) {
-			c[i] += c[i - 1];
-		}
+			// Clonando a em b:
+			int[] b = new int[array.length];
+			for (int i = 0; i < array.length; i++) {
+				b[i] = array[i];
+			}
 
-		// Clonando a em b:
-		int[] b = new int[array.length];
-		for (int i = 0; i < array.length; i++) {
-			b[i] = array[i];
-		}
+			for (int i = rightIndex; i >= leftIndex; i--) {
+				b[c[array[i] + Math.abs(array[indexMenor])] - 1 + leftIndex] = array[i];
+				c[array[i] + Math.abs(array[indexMenor])]--;
+			}
 
-		for (int i = rightIndex; i >= leftIndex; i--) {
-			b[c[array[i] + Math.abs(array[indexMenor])] - 1 + leftIndex] = array[i];
-			c[array[i] + Math.abs(array[indexMenor])]--;
-		}
-
-		for (int i = 0; i < b.length; i++) {
-			array[i] = b[i];
-		}
-
-	}
-
-	public void countingSort(Integer[] array, int leftIndex, int rightIndex) {
-		if (rightIndex >= array.length || leftIndex >= rightIndex || array == null) {
-			return;
-		}
-
-		int indexMaior = indexMaior(array, leftIndex, rightIndex);
-
-		int[] c = new int[array[indexMaior] + 1];
-		for (int i = leftIndex; i <= rightIndex; i++) {
-			c[array[i]]++;
-		}
-
-		for (int i = 1; i < c.length; i++) {
-			c[i] += c[i - 1];
-		}
-
-		int[] b = new int[array.length];
-		for (int i = 0; i < array.length; i++) {
-			b[i] = array[i];
-		}
-
-		for (int i = rightIndex; i >= leftIndex; i--) {
-			b[c[array[i]] + leftIndex - 1] = array[i];
-			c[array[i]]--;
-		}
-
-		for (int i = 0; i < b.length; i++) {
-			array[i] = b[i];
+			for (int i = 0; i < b.length; i++) {
+				array[i] = b[i];
+			}
 		}
 
 	}
