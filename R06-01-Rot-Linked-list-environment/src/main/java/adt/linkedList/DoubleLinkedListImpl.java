a35439857e4a1fa1b271f9f141e397e0b9b968d2
Lucas Leones <lucas.leones.santos@ccc.ufcg.edu.br>
2022-11-22 10:00:04 -0300

    incio double recursivo

--- R06-01-Rot-Linked-list-environment/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
+++ R06-01-Rot-Linked-list-environment/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
@@ -6,9 +6,50 @@ public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
 	protected DoubleLinkedListNode<T> last;
 
 	@Override
+	public void insert(T element) {
+		if (element != null) {
+			DoubleLinkedListNode<T> newNode = new DoubleLinkedListNode<T>(element, new DoubleLinkedListNode<T>(),
+					this.last);
+
+			if (!(this.isEmpty())) {
+				this.last.setNext(newNode);
+			} else {
+				this.head = newNode;
+			}
+			this.last = newNode;
+
+		}
+	}
+
+	@Override
+	public void remove(T element) {
+		if (element != null && !(this.isEmpty())) {
+			DoubleLinkedListNode<T> node = (DoubleLinkedListNode<T>) this.getHead();
+
+			while (!(node.isNIL()) && !(node.getData().equals(element))) {
+				node = (DoubleLinkedListNode<T>) node.getNext();
+			}
+
+			((DoubleLinkedListNode<T>) node).getPrevious().setNext(node.getNext());
+			((DoubleLinkedListNode<T>) node.getNext()).setPrevious(node.getPrevious());
+
+		}
+	}
+
+	@Override
 	public void insertFirst(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (element != null) {
+			DoubleLinkedListNode<T> no = new DoubleLinkedListNode<T>(element, new DoubleLinkedListNode<T>(),
+					new DoubleLinkedListNode<T>());
+
+			if (this.isEmpty()) {
+				super.setHead(no);
+			} else {
+				no.setNext(this.head);
+				((DoubleLinkedListNode<T>) head).setPrevious(no);
+				head = no;
+			}
+		}
 	}
 
 	@Override
